b6bd9d8e49d4c9178a9ae4fee3f391dcf2b3bc24
Anton Semenov <cemenoff@gmail.com>
2017-09-25 13:02:17 +0300

    First version of working app
    First lesson. And first version of working app.

--- StanfordMyCalculator/StanfordMyCalculator/ViewController.swift
+++ StanfordMyCalculator/StanfordMyCalculator/ViewController.swift
@@ -10,7 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Display
+    @IBOutlet weak var display: UILabel!
+    
+    //Learn where user is begining of typing
+    var userIsInTheMiddleOfTyping = false
+    
+    //Buttons
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        //If user is typing or not
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
 
+    //Move display from string to double
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    
+    //Operational buttons (+ - * / π ...)
+    @IBAction func perfomOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathSymbol = sender.currentTitle {
+            switch mathSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
     
 }
 
